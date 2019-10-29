8a6c4d3e316ecafb345d61989d81cc0234f57e52
Brandon Young <837096556@qq.com>
2019-10-29 09:47:16 +0800

    增加
    增加了计算结果导出成word;
    增加了绘图；
    增加了计算结果导入（后删）

--- gui_gui.m
+++ gui_gui.m
@@ -22,7 +22,7 @@ function varargout = gui_gui(varargin)
 
 % Edit the above text to modify the response to help gui_gui
 
-% Last Modified by GUIDE v2.5 26-Oct-2019 16:06:19
+% Last Modified by GUIDE v2.5 29-Oct-2019 09:44:01
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -250,9 +250,7 @@ function pushbutton4_1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton4_1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-clear file;
-clear path;
-clear file_path;
+clear file path file_path;
 [file,path] = uigetfile('*.xlsx'); %浏览文件
 file_suffix0 = file(end-5:end);
 file_suffix = file_suffix0(strfind(file_suffix0,'.'):end);  %判断文件类型
@@ -268,10 +266,7 @@ function pushbutton4_2_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton4_2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-clear table_data;clear data_cell;clear filter;
-clear hangshu;clear lieshu;clear hang;clear lie;
-clear Filename;clear Pathname;clear str;
-clear CloumnName;clear dataExcel;
+clear table_data data_cell filter hangshu lieshu hang lie Filename Pathname str CloumnName dataExcel;
 
 table_data = get(handles.uitable4,'Data');  %table_data含两个相同的元胞数组（2×1 cell 数组）
 data_cell = cell2mat(table_data(1,1));    %转换元胞数组
@@ -321,26 +316,139 @@ function pushbutton12_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton12 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-clear data_cell;clear filter;clear m;clear n;
-clear hangshu;clear lieshu;clear hang;clear lie;
-clear Filename;clear Pathname;clear str;
-clear CloumnName;clear dataExcel;
+clear data_cell filter m  n hangshu lieshu hang  lie Filename Pathname str CloumnName dataExcel;
 
 data_cell = get(handles.uitable5,'Data');    %转换元胞数组
 [m,n] = size(data_cell);
 if(m~=0 & n~=0)
-    filter = {'*.xlsx';'*.xls';'*.txt';'*.docx';'*.*'};
-    [Filename,Pathname] = uiputfile(filter,'另存为','data.xlsx');  %创建文件保存对话框
+    filter = {'*.docx';'*.*'};
+    [Filename,Pathname] = uiputfile(filter,'另存为','data.docx');  %创建文件保存对话框
     if (Filename==0 & Pathname==0)
         msgbox('您没有保存数据!','确认','error');
     else
             str=[Pathname Filename];
-            CloumnName=get(handles.uitable5,'ColumnName')           %获取表格的列名             
-            dataExcel=cell(size(data_cell,1)+1,size(data_cell,2));
-            dataExcel(1,:)=CloumnName;                                            %存入表格列名；
-            dataExcel(2:end,:)=num2cell(data_cell);                                              %获取表格数据；
-            xlswrite(str,dataExcel);                                              %将新单元数组写入指定的EXCEl文件中；
-            msgbox('保存数据完毕！','确认','warn');
+            
+            %生成Excel表格
+%             CloumnName=get(handles.uitable5,'ColumnName');           %获取表格的列名             
+%             dataExcel=cell(size(data_cell,1)+1,size(data_cell,2));
+%             dataExcel(1,:)=CloumnName;                                            %存入表格列名；
+%             dataExcel(2:end,:)=num2cell(data_cell);                                              %获取表格数据；
+%             xlswrite(str,dataExcel);                                              %将新单元数组写入指定的EXCEl文件中；
+%             msgbox('保存数据完毕！','确认','warn');
+
+            %生成Word文档
+            filespec_user = [str];  % 设定测试Word文件名和路径
+            % 判断Word是否已经打开，若已打开，就在打开的Word中进行操作，否则就打开Word
+            try
+                % 若Word服务器已经打开，返回其句柄Word
+                Word = actxGetRunningServer('Word.Application');
+            catch
+            % 否则，创建一个Microsoft Word服务器，返回句柄Word
+                Word = actxserver('Word.Application');
+            end;
+            Word.Visible = 1; % 或set(Word, 'Visible', 1);
+
+            % 若测试文件存在，打开该测试文件，否则，新建一个文件，并保存，文件名为测试.docx
+            if exist(filespec_user,'file');
+                Document = Word.Documents.Open(filespec_user);
+            % Document = invoke(Word.Documents,'Open',filespec_user);
+            else
+                Document = Word.Documents.Add;
+            % Document = invoke(Word.Documents, 'Add');
+                Document.SaveAs2(filespec_user);
+            end
+
+            % 设定光标位置从头开始
+            Content = Document.Content;
+            Selection = Word.Selection;
+            Paragraphformat = Selection.ParagraphFormat;
+
+            % 设定页面大小
+            Document.PageSetup.TopMargin = 60; % 单位像素
+            Document.PageSetup.BottomMargin = 45;
+            Document.PageSetup.LeftMargin = 45;
+            Document.PageSetup.RightMargin = 45;
+
+            % Content.InsertParagraphAfter;% 插入一段
+            % Content.Collapse=0; % 0为不覆盖
+            Content.Start = 0;
+            title = '拉力扭矩预测';
+            Content.Text = title;
+            Content.Font.Size = 22 ;
+            Content.Font.Bold = 4 ;
+            Content.Paragraphs.Alignment = 'wdAlignParagraphCenter';% 设定段落格式
+            Selection.Start = Content.end;% 定义开始的位置
+            Selection.TypeParagraph;
+
+            % 插入内容并定义字体字号
+            smallTitle = '大庆油田有限责任公司试油试采分公司';
+            Selection.Text = smallTitle;
+            Selection.Font.Size = 12;
+            Selection.Font.Bold = 0; 
+            Selection.MoveDown;
+            ParagraphFormat.Alignment = 'wdAlignParagraphCenter';
+            Selection.TypeParagraph;    %插入新的空段落
+            Selection.Font.Size = 10;
+
+            %项目名称
+            smallTitle = '项目名称：齐平1井钻桥塞';
+            Selection.Text = smallTitle;
+            Selection.Font.Size = 12;
+            Selection.Font.Bold = 0; 
+            Selection.MoveDown;
+            Selection.ParagraphFormat.Alignment = 'wdAlignParagraphLeft';
+            Selection.TypeParagraph;    %插入新的空段落
+            
+            %绘制表格
+            Selection.ParagraphFormat.Alignment = 'wdAlignParagraphCenter'; %设置表格文字居中
+            Selection.Font.Size = 10;   %设置表格中字体大小为10
+            Tables = Document.Tables.Add(Selection.Range,size(data_cell,1),10);    % ”data_cell+1“ 行 10列
+            DTI = Document.Tables.Item(1); % 或DTI = Tables;
+            DTI.Borders.OutsideLineStyle = 'wdLineStyleSingle';% 设置外边框的线型，Dash，DashDot,DashDotDot,DashSmallGap,DashLargeGap,Dot,Double,Triple等
+            DTI.Borders.OutsideLineWidth = 'wdLineWidth150pt';% 设置线宽，有025，050，075，100，150，225，300，450，600pt等
+            DTI.Borders.InsideLineStyle = 'wdLineStyleSingle';%设置内边框的线型
+            DTI.Borders.InsideLineWidth = 'wdLineWidth075pt';
+            DTI.Rows.Alignment = 'wdAlignRowCenter';%设置行对齐方式
+
+            % DTI.Rows.Item(8).Borders.Item(1).LineStyle = 'wdLineStyleNone';% 设置第8行上边界线型，1，2，3，4分别对应单元格的上、左、下、右边界
+            % DTI.Rows.Item(8).Borders.Item(3).LineStyle = 'wdLineStyleNone';% 设置第8行下边界线型
+            % DTI.Rows.Item(11).Borders.Item(1).LineStyle = 'wdLineStyleNone';
+            % DTI.Rows.Item(11).Borders.Item(3).LineStyle = 'wdLineStyleNone';
+
+            column_width = [53.7736,85.1434,53.7736,35.0094,35.0094,76.6981,55.1887,52.9245,54.9057];% 设置列宽，单位为磅
+            row_height = [28.5849,28.5849,28.5849,28.5849,25.4717,25.4717,32.8302,312.1698,17.8302,49.2453,14.1509,18.6792]; % 设置行高
+
+            % 指定各单元格内容
+            DTI.Cell(1,1).Range.Text = '序号';
+            DTI.Cell(1,2).Range.Text = '测深(m)';
+            DTI.Cell(1,3).Range.Text = '井斜角(°)';
+            DTI.Cell(1,4).Range.Text = '方位角(°)';
+            DTI.Cell(1,5).Range.Text = '拉力';
+            DTI.Cell(1,6).Range.Text = '扭矩(kN·m)';
+            DTI.Cell(1,7).Range.Text = '接触压(kN/m)';
+            DTI.Cell(1,8).Range.Text = '稳定性';
+            DTI.Cell(1,9).Range.Text = '安全系数';
+            DTI.Cell(1,10).Range.Text = '伸长(m)';
+%             DTI.Cell(1,10).Range.Font.Size = 10;
+
+            %填写数据
+            for cell_word = 1:size(data_cell,1)-1
+                DTI.Cell(cell_word+1,1).Range.Text = num2str(cell_word);                            %序号 
+                DTI.Cell(cell_word+1,2).Range.Text = num2str(data_cell{cell_word+1,1});      %测深    
+                DTI.Cell(cell_word+1,3).Range.Text = num2str(data_cell{cell_word+1,2});     %井斜角
+                DTI.Cell(cell_word+1,4).Range.Text = num2str(data_cell{cell_word+1,3});     %方位角
+                DTI.Cell(cell_word+1,5).Range.Text = num2str(data_cell{cell_word+1,11});    %拉力   
+                DTI.Cell(cell_word+1,6).Range.Text = num2str(data_cell{cell_word+1,12});    %扭矩  
+                DTI.Cell(cell_word+1,7).Range.Text = num2str(data_cell{cell_word+1,13});    %接触压           
+                DTI.Cell(cell_word+1,8).Range.Text = num2str(data_cell{cell_word+1,14});   %稳定性              
+                DTI.Cell(cell_word+1,9).Range.Text = num2str(data_cell{cell_word+1,16});    %安全系数             
+                DTI.Cell(cell_word+1,10).Range.Text = num2str(data_cell{cell_word+1,17});   %伸长
+            end
+
+            Document.ActiveWindow.ActivePane.View.Type = 'wdPrintView'; % 设置视图方式为页面
+            Document.Save; % 保存文档
+%             Document.Close; % 关闭文档
+%             Word.Quit; % 退出word服务器
     end
 else
     msgbox('保存的数据不能为空！','确认','error');
@@ -356,12 +464,12 @@ clear table_data1;clear table_data2;clear table_data3;clear table_data4;
 clear data_cell;clear data_cel2;clear data_cel3;clear data_cel4;
 clear hang1;clear lie1;clear hang2;clear lie2;
 clear hang3;clear lie3;clear hang4;clear lie4;
-clear j;
+clear j;clear hang;
+
+%测深&井斜角 计算
 j = 1;
 table_data1 = get(handles.uitable1,'Data');
-
 data_cell = cell2mat(table_data1); 
-
 for hang1=1:size(data_cell,1)
     table1_data1 = get(handles.uitable1(1,1),'Data');
     disp(table1_data1);
@@ -388,6 +496,50 @@ for hang1=1:size(data_cell,1)
     end
 end
 
+% %方位角
+% 
+% %地层温度
+% 
+% %井筒直径
+% set(handles.uitable5(:,5),'Data','0.150'); 
+% % for hang = 1:size(data_cell,1)
+% %     set(handles.uitable5(hang,5),'Data','0.150'); 
+% % end
+% %管柱外径
+% for hang = 1:size(data_cell,1)
+%     set(handles.uitable5(hang,6),'Data','0.073'); 
+% end
+% %管柱内径
+% for hang = 1:size(data_cell,1)
+%     set(handles.uitable5(hang,7),'Data','0.057'); 
+% end
+% %环空流体密度
+% for hang = 1:size(data_cell,1)
+%     set(handles.uitable5(hang,8),'Data','1150.000'); 
+% end
+% %环空流体黏度
+% 
+% %管内流体
+% 
+% %轴向拉力
+% 
+% %扭矩
+% for hang = 1:size(data_cell,1)
+%     set(handles.uitable5(hang,12),'Data','0'); 
+% end
+% %接触压力
+% 
+% %稳定性状
+% for hang = 1:size(data_cell,1)
+%     set(handles.uitable5(hang,14),'Data','稳定'); 
+% end
+% %应力强度
+% 
+% %安全系数
+% for hang = 1:size(data_cell,1)
+%     set(handles.uitable5(hang,16),'Data','10.000'); 
+% end
+% %伸长
 
 % --- Executes during object creation, after setting all properties.
 function uipanel7_CreateFcn(hObject, eventdata, handles)
@@ -428,3 +580,154 @@ function text11_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to text11 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes on button press in pushbutton14.
+function pushbutton14_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clear file;
+clear path;
+clear file_path;
+[file,path] = uigetfile('*.xlsx'); %浏览文件
+file_suffix0 = file(end-5:end);
+file_suffix = file_suffix0(strfind(file_suffix0,'.'):end);  %判断文件类型
+clear file_suffix0;
+file_path = strcat(path,file)
+if(file_suffix == '.xlsx') 
+    [num,txt,raw]=xlsread(file_path);    %读取Excel文件
+    set(handles.uitable5,'Data',[raw]);
+end
+
+
+% --- Executes when selected cell(s) is changed in uitable1.
+function uitable1_CellSelectionCallback(hObject, eventdata, handles)
+% hObject    handle to uitable1 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) currently selecteds
+% handles    structure with handles and user data (see GUIDATA)
+%行列索引
+global row;
+global col;
+index=eventdata.Indices
+row=index(1);
+col=index(2);
+
+
+% --- Executes on button press in pushbutton15.
+function pushbutton15_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton16.
+function pushbutton16_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton16 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clear newData;
+global row col;
+if(row>0)
+    newData = get(handles.uitable1,'Data');  %获取表格数据矩阵
+    newData(row,:) = [];   %删除选中的某行数据
+    set(handles.uitable1,'Data',newData);  %显示到表格中
+else
+    msgbox('请先选择要删除的行！','确认','error');
+end
+
+% --- Executes on button press in pushbutton17.
+function pushbutton17_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton17 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton18.
+function pushbutton18_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton18 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clear newData;
+global row col;
+if(row>0)
+    newData = get(handles.uitable1,'Data');  %获取表格数据矩阵
+    newData(row,:) = [];   %删除选中的某行数据
+    set(handles.uitable1,'Data',newData);  %显示到表格中
+else
+    msgbox('请先选择要删除的行！','确认','error');
+end
+
+% --- Executes on button press in pushbutton19.
+function pushbutton19_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton19 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clear newData;
+global row col;
+if(row>0)
+    newData = get(handles.uitable1,'Data');  %获取表格数据矩阵
+    newData(row,:) = [];   %删除选中的某行数据
+    set(handles.uitable1,'Data',newData);  %显示到表格中
+else
+    msgbox('请先选择要删除的行！','确认','error');
+end
+
+% --- Executes on button press in pushbutton20.
+function pushbutton20_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton20 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton21.
+function pushbutton21_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton21 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton22.
+function pushbutton22_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton22 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clear newData;
+global row col;
+if(row>0)
+    newData = get(handles.uitable1,'Data');  %获取表格数据矩阵
+    newData(row,:) = [];   %删除选中的某行数据
+    set(handles.uitable1,'Data',newData);  %显示到表格中
+else
+    msgbox('请先选择要删除的行！','确认','error');
+end
+
+
+% --- Executes on button press in pushbutton23.
+function pushbutton23_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton23 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+table_data3 = get(handles.uitable5(1,1),'Data');
+data3_1 = table_data3(:,1);
+data3_2 = table_data3(:,2);
+x = cell2mat(data3_1);
+y = cell2mat(data3_2);
+disp(x);
+disp(y);
+axes(handles.axes1); 
+plot(x,y);
+
+% --- Executes on button press in pushbutton24.
+function pushbutton24_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton24 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton25.
+function pushbutton25_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton25 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
